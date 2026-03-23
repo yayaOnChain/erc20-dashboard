e01b8c8e1eb0ce6618fba8b09337d2d740e6c516
yayaOnChain <yaya.nurcahya@gmail.com>
2026-03-23 21:39:05 +0700

    Update project config

---
+++ hardhat.config.ts
@@ -0,0 +1,31 @@
+import { HardhatUserConfig } from "hardhat/config";
+
+const config: HardhatUserConfig = {
+  solidity: {
+    version: "0.8.20",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  networks: {
+    hardhat: {
+      chainId: 31337,
+    },
+    sepolia: {
+      url: process.env.SEPOLIA_RPC_URL || "https://rpc.sepolia.org",
+      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+      chainId: 11155111,
+    },
+  },
+  paths: {
+    sources: "./src/contracts",
+    tests: "./src/test",
+    cache: "./cache/hardhat",
+    artifacts: "./src/artifacts",
+  },
+};
+
+export default config;
