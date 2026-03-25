252e84dc32b64bc288b0ceac151e906afff567b7
yayaOnChain <yaya.nurcahya@gmail.com>
2026-03-25 23:12:09 +0700

    Updated types

--- src/frontend/utils/types.ts
+++ src/frontend/utils/types.ts
@@ -4,9 +4,9 @@ declare global {
   interface Window {
     ethereum?: Eip1193Provider & {
       isMetaMask?: boolean;
-      request: (args: { method: string; params?: any[] }) => Promise<any>;
-      on: (event: string, callback: (...args: any[]) => void) => void;
-      removeListener: (event: string, callback: (...args: any[]) => void) => void;
+      request: (args: { method: string; params?: unknown[] }) => Promise<unknown>;
+      on: (event: string, callback: (...args: unknown[]) => void) => void;
+      removeListener: (event: string, callback: (...args: unknown[]) => void) => void;
     };
   }
 }
