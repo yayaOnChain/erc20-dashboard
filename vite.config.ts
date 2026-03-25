dc554865c987a207f7f446ecef8fa7b2806ab3bc
yayaOnChain <yaya.nurcahya@gmail.com>
2026-03-25 22:44:57 +0700

    Updated vite config to read env

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,19 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-  define: {
-    'process.env.VITE_CONTRACT_ADDRESS': JSON.stringify(process.env.VITE_CONTRACT_ADDRESS || ''),
-    'process.env.VITE_CONTRACT_CHAIN_ID': JSON.stringify(process.env.VITE_CONTRACT_CHAIN_ID || ''),
-  },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  
+  return {
+    plugins: [
+      react(),
+      tailwindcss(),
+    ],
+    define: {
+      'process.env.VITE_CONTRACT_ADDRESS': JSON.stringify(env.VITE_CONTRACT_ADDRESS || ''),
+      'process.env.VITE_CONTRACT_CHAIN_ID': JSON.stringify(env.VITE_CONTRACT_CHAIN_ID || ''),
+    },
+  }
 })
